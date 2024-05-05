0c29888bcfd84953fa9a58403a1b9d243ccf2c1e
merteyup <eupmrt@gmail.com>
2024-05-05 22:01:46 +0300

    Initial Commit

---
+++ Remote-Work/Remote_WorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Remote_WorkApp.swift
+//  Remote-Work
+//
+//  Created by Eyüp Mert on 5.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Remote_WorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
